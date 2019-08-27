789d8431ed0ca3afeb3e67815dd5323d28a39a1d
erisjimver <erisjinver@gmail.com>
2019-08-27 09:35:13 -0500

    configurar estado de lectura e inclusxion de archivo conectar

--- editar.php
+++ editar.php
@@ -9,7 +9,17 @@
 <body>
 
 <h1>ACTUALIZAR</h1>
+<?php 
 
+  $id=$_GET["id"];
+  $nombre=$_GET["nom"];
+  $apellido=$_GET["ape"];
+  $direccion=$_GET["dir"];
+
+
+
+
+?>
 <p>
  
 </p>
@@ -19,22 +29,22 @@
     <tr>
       <td></td>
       <td><label for="id"></label>
-      <input type="hidden" name="id" id="id"></td>
+      <input type="hidden" name="id" id="id" value="<?php echo $id ?>"></td>
     </tr>
     <tr>
       <td>Nombre</td>
       <td><label for="nom"></label>
-      <input type="text" name="nom" id="nom"></td>
+      <input type="text" name="nom" id="nom" value="<?php echo $nombre ?>"></td>
     </tr>
     <tr>
       <td>Apellido</td>
       <td><label for="ape"></label>
-      <input type="text" name="ape" id="ape"></td>
+      <input type="text" name="ape" id="ape" value="<?php echo $apellido ?>"></td>
     </tr>
     <tr>
       <td>Dirección</td>
       <td><label for="dir"></label>
-      <input type="text" name="dir" id="dir"></td>
+      <input type="text" name="dir" id="dir" value="<?php echo $direccion ?>"></td>
     </tr>
     <tr>
       <td colspan="2"><input type="submit" name="bot_actualizar" id="bot_actualizar" value="Actualizar"></td>
